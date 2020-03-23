3101c7bf90aab762db200328ee218c77f5e762d3
jacekplonka <39088814+jacekplonka@users.noreply.github.com>
2020-03-23 21:28:56 +0100

    Update zaloguj.bat

--- zaloguj.bat
+++ zaloguj.bat
@@ -1,8 +1,9 @@
 @echo off
 
-SET %minutes = %4% * 60
+SET /A %seconds=60
+SET /A %minutes=%4%*%seconds%
 
 FOR /L %%X IN (0, 1, %3) DO (
-    python3 source/login.py %1 %2
+    python source/login.py %1 %2
     timeout /t %minutes
 )
